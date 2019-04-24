aceef1d875155bbe1e9d67b720394a8071f7d251
Knut Melvær <knut.melvaer@gmail.com>
2019-04-24 08:10:08 +0200

    Add times for sessions

---
+++ template/web/components/Sessions.vue
@@ -0,0 +1,55 @@
+<template>
+  <ul class="sessions">
+    <SessionItem
+      v-for="(scheduleItem, index) in schedule"
+      :key="scheduleItem._key"
+      :index="index"
+      :schedule-item="scheduleItem"
+    />
+  </ul>
+</template>
+<script>
+import { addMinutes } from 'date-fns'
+import SessionItem from '~/components/SessionItem'
+export default {
+  components: {
+    SessionItem
+  },
+  props: {
+    info: {
+      from: {
+        type: Date,
+        default: () => new Date()
+      }
+    },
+    program: {
+      schedule: {
+        type: Array,
+        default: () => []
+      }
+    }
+  },
+  data: function() {
+    return {
+      /**
+       * Calculates session time slot by looping trough all sessions
+       * adding their duration to their start time.
+       */
+      schedule: this.program.schedule.reduce((allSessions, session, index) => {
+        const prevSession = allSessions[index - 1]
+        const fromTime = prevSession
+          ? addMinutes(prevSession.fromTime, prevSession.duration)
+          : this.info.from
+        return allSessions.concat([{ ...session, fromTime }])
+      }, [])
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul.sessions {
+  margin: 0;
+  padding: 0;
+}
+</style>
