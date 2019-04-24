1620492b7cbe9fc344314cfddaaa517dc5ce535e
Kristoffer Sivertsen <kristoffer@sanity.io>
2019-04-24 17:48:35 +0200

    Code of conduct style and serializers

---
+++ template/web/components/blockContent/PersonBlock.vue
@@ -0,0 +1,34 @@
+<template>
+  <span>
+    <nuxt-link v-if="currentSlug" :to="{ path: `/speakers/${currentSlug}` }">
+      <b>{{ personName }} </b>
+    </nuxt-link>
+    <b v-if="!currentSlug">{{ personName }} </b>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    person: {
+      type: Object,
+      default: () => {
+        return { name: 'Unknown' }
+      }
+    }
+  },
+  computed: {
+    personName: props => {
+      console.log(props.person)
+      return props.person.name
+    },
+    currentSlug: ({ person }) => {
+      const { slug } = person
+      console.log('slug', slug)
+      if (slug && slug.current) {
+        return slug.current
+      }
+    }
+  }
+}
+</script>
