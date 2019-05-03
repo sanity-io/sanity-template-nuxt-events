2812dcdebdde4d3cd135e07f11fa9f70f21d664f
Marius Lundgård <studio@mariuslundgard.com>
2019-05-03 14:45:43 +0200

    Fix document list widget

--- template/studio/dashboardConfig.js
+++ template/studio/dashboardConfig.js
@@ -42,7 +42,7 @@ export default {
     {name: 'project-users', layout: {height: 'auto'}},
     {
       name: 'document-list',
-      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      options: {title: 'Recently created sessions', order: '_createdAt desc', types: ['session']},
       layout: {width: 'medium'}
     }
   ]
