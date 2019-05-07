f20361865402dac05adc7b7206dd4d2b3569b89d
Knut Melvær <knut.melvaer@gmail.com>
2019-05-07 11:07:01 +0200

    Fix schema name and order

--- template/studio/schemas/schema.js
+++ template/studio/schemas/schema.js
@@ -4,7 +4,7 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
-// document schemas
+// document types
 import eventInformation from './documents/eventInformation'
 import person from './documents/person'
 import codeOfConduct from './documents/codeOfConduct'
@@ -24,27 +24,27 @@ import personReference from './objects/personReference'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'blog',
+  name: 'event',
   // Then proceed to concatenate our our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
+    codeOfConduct,
     eventInformation,
-    venue,
-    schedule,
     person,
+    venue,
     session,
+
+    // When added to this list, object types can be used as
+    // { type: 'typename' } in other document schemas
+    schedule,
     program,
     scheduleItem,
-    codeOfConduct,
     mainImage,
     bioPortableText,
     bodyPortableText,
     eventReference,
-    personReference
-
-    // When added to this list, object types can be used as
-    // { type: 'typename' } in other document schemas
+    personReference,
   ])
 })
