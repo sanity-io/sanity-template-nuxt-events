ff36ae1fa120a1cd230719b61c5efe5476d14c49
Knut Melvær <knut@sanity.io>
2019-05-28 22:46:46 +0200

    Add settings

--- template/studio/schemas/documents/program.js
+++ template/studio/schemas/documents/program.js
@@ -2,6 +2,7 @@ export default {
   name: 'program',
   type: 'document',
   title: 'Program',
+  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
   fields: [
     {
       name: 'schedule',
